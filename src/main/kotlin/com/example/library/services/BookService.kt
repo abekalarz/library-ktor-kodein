efd7b91faa520e40dcce765b9d8b005c3f2b5e8b
abekalarz-oec <Adam.Bekalarz@oeconnection.com>
2025-12-03 14:41:14 +0100

    dirty try to replace direct JDBI/DatabaseFactory usage with 'Respository' clases -> first example for 'BookService' and BookRepository.kt

--- src/main/kotlin/com/example/library/services/BookService.kt
+++ src/main/kotlin/com/example/library/services/BookService.kt
@@ -1,24 +1,30 @@
 package com.example.library.services
 
 import com.example.library.db.DatabaseFactory
+import com.example.library.repository.BookRepository
 
 data class Book(val id: Int, val title: String, val available: Boolean)
 
-class BookService(private val db: DatabaseFactory) {
-    fun listBooks(titleFilter: String? = null): List<Book> = db.jdbi.withHandle<List<Book>, Exception> { handle ->
-        val sql = if (titleFilter != null)
-            "SELECT * FROM books WHERE title LIKE CONCAT('%', ?, '%')" else "SELECT * FROM books"
-        handle.createQuery(sql).apply {
-            if (titleFilter != null) bind(0, titleFilter)
-        }.map { rs, _ ->
-            Book(
-                id = rs.getInt("id"),
-                title = rs.getString("title"),
-                available = rs.getBoolean("available")
-            )
-        }.list()
-    }
-    
+class BookService(
+    private val db: DatabaseFactory,
+    private val bookRepository: BookRepository
+) {
+//    fun listBooks(titleFilter: String? = null): List<Book> = db.jdbi.withHandle<List<Book>, Exception> { handle ->
+//        val sql = if (titleFilter != null)
+//            "SELECT * FROM books WHERE title LIKE CONCAT('%', ?, '%')" else "SELECT * FROM books"
+//        handle.createQuery(sql).apply {
+//            if (titleFilter != null) bind(0, titleFilter)
+//        }.map { rs, _ ->
+//            Book(
+//                id = rs.getInt("id"),
+//                title = rs.getString("title"),
+//                available = rs.getBoolean("available")
+//            )
+//        }.list()
+//    }
+
+    fun listBooks(titleFilter: String? = null): List<Book> = bookRepository.listBooks(titleFilter)
+
     fun addBook(title: String): Int = db.jdbi.withHandle<Int, Exception> { handle ->
         handle.createUpdate("INSERT INTO books (title, available) VALUES (?, true)")
             .bind(0, title)
